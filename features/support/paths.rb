363d68e7f7dd9361b771487bd5ed12232bf39106
Anjali Chadha <anjali.chadha229@gmail.com>
2018-10-12 03:46:01 +0000

    Adding one more Cucumber test case for categories

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -17,13 +17,14 @@ module NavigationHelpers
       '/'
     when /^the new article page$/
       '/admin/content/new'
+    when /^the categories page$/
+     '/admin/categories/new'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
-
     else
       begin
         page_name =~ /^the (.*) page$/
